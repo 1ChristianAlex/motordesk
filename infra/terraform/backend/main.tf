faca9a7ff786e7e285ab07ed8879c91ea694fc24
Christian Alexsander Santos <47725581+1ChristianAlex@users.noreply.github.com>
2026-08-30 20:32:30 -0300

    refactor(terraform): harden remote state storage

--- infra/terraform/backend/main.tf
+++ infra/terraform/backend/main.tf
@@ -12,18 +12,39 @@ provider "azurerm" {
 }
 
 variable "project_name" {
-  type    = string
-  default = "motordesk"
+  description = "Project name used as a resource prefix."
+  type        = string
+  default     = "motordesk"
 }
 
 variable "environment" {
-  type    = string
-  default = "dev"
+  description = "Environment used to isolate the Terraform state backend."
+  type        = string
+  default     = "dev"
+}
+
+variable "infra_location" {
+  description = "Azure region for the persistent Terraform backend resources."
+  type        = string
+  default     = "brazilsouth"
+}
+
+variable "tags" {
+  description = "Tags applied to persistent Terraform backend resources."
+  type        = map(string)
+
+  default = {
+    project     = "motordesk"
+    managed_by  = "terraform"
+    environment = "dev"
+    purpose     = "terraform-state"
+  }
 }
 
 resource "azurerm_resource_group" "resource_persistent" {
   name     = "rg-persistent-${var.project_name}-${var.environment}"
-  location = "brazilsouth"
+  location = var.infra_location
+  tags     = var.tags
 }
 
 resource "azurerm_storage_account" "tfstate_storage_account" {
@@ -33,23 +54,28 @@ resource "azurerm_storage_account" "tfstate_storage_account" {
   account_tier                    = "Standard"
   account_replication_type        = "LRS"
   min_tls_version                 = "TLS1_2"
+  https_traffic_only_enabled      = true
   allow_nested_items_to_be_public = false
+  tags                            = var.tags
 }
 
 resource "azurerm_storage_container" "tfstate_storage_account" {
-  name                  = "tfstate"
-  storage_account_id    = azurerm_storage_account.tfstate_storage_account.id
+  name               = "tfstate"
+  storage_account_id = azurerm_storage_account.tfstate_storage_account.id
   container_access_type = "private"
 }
 
 output "resource_group_name" {
-  value = azurerm_resource_group.resource_persistent.name
+  description = "Resource group containing the Terraform state storage."
+  value       = azurerm_resource_group.resource_persistent.name
 }
 
 output "storage_account_name" {
-  value = azurerm_storage_account.tfstate_storage_account.name
+  description = "Storage account containing the Terraform state."
+  value       = azurerm_storage_account.tfstate_storage_account.name
 }
 
 output "container_name" {
-  value = azurerm_storage_container.tfstate_storage_account.name
+  description = "Blob container containing Terraform state files."
+  value       = azurerm_storage_container.tfstate_storage_account.name
 }
