5b228e6a005a7a1ec5fc9a586b7c6bb84d7e705a
Christian Alexsander Santos <47725581+1ChristianAlex@users.noreply.github.com>
2026-08-31 09:21:26 -0300

    refactor(terraform): move key vault bootstrap out of main state

--- infra/terraform/azure/main.tf
+++ infra/terraform/azure/main.tf
@@ -18,9 +18,11 @@ terraform {
     }
   }
 }
+
 provider "random" {
   # Configuration options
 }
+
 # Configure the Microsoft Azure Provider
 provider "azurerm" {
   features {
@@ -40,10 +42,12 @@ provider "azapi" {
   skip_provider_registration = false
 }
 
-data "azurerm_client_config" "client_config" {
+data "azurerm_client_config" "client_config" {}
 
+data "azurerm_key_vault" "kvault_app" {
+  name                = "kv${var.environment}-${var.project_name}"
+  resource_group_name = azurerm_resource_group.rg.name
 }
-
 
 # Create a resource group
 resource "azurerm_resource_group" "rg" {
@@ -65,44 +69,6 @@ resource "azurerm_role_assignment" "appconf_dataowner" {
   role_definition_name = "App Configuration Data Owner"
   principal_id         = data.azurerm_client_config.client_config.object_id
 }
-
-resource "azurerm_key_vault" "kvault_app" {
-  name                       = "kv${var.environment}-${var.project_name}"
-  location                   = azurerm_resource_group.rg.location
-  resource_group_name        = azurerm_resource_group.rg.name
-  rbac_authorization_enabled = false
-  tenant_id                  = data.azurerm_client_config.client_config.tenant_id
-  sku_name                   = "premium"
-  soft_delete_retention_days = 7
-  tags                       = var.tags
-
-  access_policy {
-    tenant_id = data.azurerm_client_config.client_config.tenant_id
-    object_id = data.azurerm_client_config.client_config.object_id
-
-    key_permissions = [
-      "Create",
-      "Delete",
-      "Get",
-      "Purge",
-      "Recover",
-      "List",
-      "Update",
-      "GetRotationPolicy",
-      "SetRotationPolicy"
-    ]
-
-    secret_permissions = [
-      "Set",
-      "Get",
-      "Delete",
-      "List",
-      "Purge",
-      "Recover"
-    ]
-  }
-}
-
 
 
 # $Env:ARM_CLIENT_ID = ""
