8b5727bae5cc8d0372418d02c2ac9eb4c53c02c6
Christian Alexsander Santos <47725581+1ChristianAlex@users.noreply.github.com>
2026-08-31 23:42:17 -0300

    refactor(terraform): reference existing key vault secrets

---
+++ infra/terraform/azure/outputs.tf
@@ -0,0 +1,212 @@
+output "acr_login_server" {
+  description = "Azure Container Registry login server."
+  value       = azurerm_container_registry.acr.login_server
+}
+
+output "acr_id" {
+  description = "Azure Container Registry resource ID."
+  value       = azurerm_container_registry.acr.id
+}
+
+resource "azurerm_app_configuration_key" "ack_database_url" {
+  configuration_store_id = azurerm_app_configuration.app_conf.id
+  key                    = "DATABASE_URL"
+  label                  = var.environment
+  type                   = "kv"
+  value                  = "jdbc:postgresql://${data.azurerm_postgresql_flexible_server.data_pg.fqdn}:5432/${azurerm_postgresql_flexible_server_database.motordesk.name}"
+  depends_on = [
+    azurerm_role_assignment.appconf_dataowner,
+    azurerm_postgresql_flexible_server.pg_db
+  ]
+}
+
+resource "azurerm_app_configuration_key" "ack_database_user" {
+  configuration_store_id = azurerm_app_configuration.app_conf.id
+  key                    = "DATABASE_USER"
+  label                  = var.environment
+  type                   = "vault"
+  vault_key_reference    = data.azurerm_key_vault_secret.database_user.versionless_id
+  depends_on = [
+    azurerm_role_assignment.appconf_dataowner
+  ]
+}
+
+resource "azurerm_app_configuration_key" "ack_database_password" {
+  configuration_store_id = azurerm_app_configuration.app_conf.id
+  key                    = "DATABASE_PASSWORD"
+  label                  = var.environment
+  type                   = "vault"
+  vault_key_reference    = data.azurerm_key_vault_secret.database_password.versionless_id
+  depends_on = [
+    azurerm_role_assignment.appconf_dataowner
+  ]
+}
+
+resource "azurerm_app_configuration_key" "ack_database_driver" {
+  configuration_store_id = azurerm_app_configuration.app_conf.id
+  key                    = "DATABASE_DRIVER"
+  label                  = var.environment
+  type                   = "kv"
+  value                  = "org.postgresql.Driver"
+  depends_on = [
+    azurerm_role_assignment.appconf_dataowner
+  ]
+}
+
+resource "azurerm_app_configuration_key" "ack_mongo_url" {
+  configuration_store_id = azurerm_app_configuration.app_conf.id
+  key                    = "MONGO_URL"
+  label                  = var.environment
+  type                   = "vault"
+  vault_key_reference    = data.azurerm_key_vault_secret.mongo_url.versionless_id
+  depends_on = [
+    azurerm_role_assignment.appconf_dataowner
+  ]
+}
+
+resource "azurerm_app_configuration_key" "ack_mongo_user" {
+  configuration_store_id = azurerm_app_configuration.app_conf.id
+  key                    = "MONGO_USER"
+  label                  = var.environment
+  type                   = "vault"
+  vault_key_reference    = data.azurerm_key_vault_secret.mongo_user.versionless_id
+  depends_on = [
+    azurerm_role_assignment.appconf_dataowner
+  ]
+}
+
+resource "azurerm_app_configuration_key" "ack_mongo_password" {
+  configuration_store_id = azurerm_app_configuration.app_conf.id
+  key                    = "MONGO_PASSWORD"
+  label                  = var.environment
+  type                   = "vault"
+  vault_key_reference    = data.azurerm_key_vault_secret.mongo_password.versionless_id
+  depends_on = [
+    azurerm_role_assignment.appconf_dataowner
+  ]
+}
+
+resource "azurerm_app_configuration_key" "ack_mongo_database" {
+  configuration_store_id = azurerm_app_configuration.app_conf.id
+  key                    = "MONGO_DATABASE"
+  label                  = var.environment
+  type                   = "vault"
+  vault_key_reference    = data.azurerm_key_vault_secret.mongo_database.versionless_id
+  depends_on = [
+    azurerm_role_assignment.appconf_dataowner
+  ]
+}
+
+resource "azurerm_app_configuration_key" "ack_redis_host" {
+  configuration_store_id = azurerm_app_configuration.app_conf.id
+  key                    = "REDIS_HOST"
+  label                  = var.environment
+  type                   = "kv"
+  value                  = "redis"
+  depends_on = [
+    azurerm_role_assignment.appconf_dataowner
+  ]
+}
+
+resource "azurerm_app_configuration_key" "ack_redis_port" {
+  configuration_store_id = azurerm_app_configuration.app_conf.id
+  key                    = "REDIS_PORT"
+  label                  = var.environment
+  type                   = "kv"
+  value                  = "6380"
+  depends_on = [
+    azurerm_role_assignment.appconf_dataowner
+  ]
+}
+
+resource "azurerm_app_configuration_key" "ack_redis_password" {
+  configuration_store_id = azurerm_app_configuration.app_conf.id
+  key                    = "REDIS_PASSWORD"
+  label                  = var.environment
+  type                   = "vault"
+  vault_key_reference    = data.azurerm_key_vault_secret.redis_password.versionless_id
+  depends_on = [
+    azurerm_role_assignment.appconf_dataowner
+  ]
+}
+
+resource "azurerm_app_configuration_key" "ack_azure_communication_access_key" {
+  configuration_store_id = azurerm_app_configuration.app_conf.id
+  key                    = "AZURE_COMMUNICATION_ACCESS_KEY"
+  label                  = var.environment
+  type                   = "vault"
+  vault_key_reference    = data.azurerm_key_vault_secret.azure_communication_access_key.versionless_id
+  depends_on = [
+    azurerm_role_assignment.appconf_dataowner
+  ]
+}
+
+resource "azurerm_app_configuration_key" "ack_azure_communication_endpoint" {
+  configuration_store_id = azurerm_app_configuration.app_conf.id
+  key                    = "AZURE_COMMUNICATION_ENDPOINT"
+  label                  = var.environment
+  type                   = "vault"
+  vault_key_reference    = data.azurerm_key_vault_secret.azure_communication_endpoint.versionless_id
+  depends_on = [
+    azurerm_role_assignment.appconf_dataowner
+  ]
+}
+
+resource "random_string" "ack_jwt_issuer" {
+  length  = 16
+  special = true
+}
+
+resource "azurerm_app_configuration_key" "ack_jwt_issuer" {
+  configuration_store_id = azurerm_app_configuration.app_conf.id
+  key                    = "JWT_ISSUER"
+  label                  = var.environment
+  type                   = "kv"
+  value                  = random_string.ack_jwt_issuer.result
+  depends_on = [
+    azurerm_role_assignment.appconf_dataowner
+  ]
+}
+
+resource "random_string" "ack_jwt_audience" {
+  length  = 16
+  special = true
+}
+
+resource "azurerm_app_configuration_key" "ack_jwt_audience" {
+  configuration_store_id = azurerm_app_configuration.app_conf.id
+  key                    = "JWT_AUDIENCE"
+  label                  = var.environment
+  type                   = "kv"
+  value                  = random_string.ack_jwt_audience.result
+  depends_on = [
+    azurerm_role_assignment.appconf_dataowner
+  ]
+}
+
+resource "random_string" "ack_jwt_realm" {
+  length  = 16
+  special = true
+}
+
+resource "azurerm_app_configuration_key" "ack_jwt_realm" {
+  configuration_store_id = azurerm_app_configuration.app_conf.id
+  key                    = "JWT_REALM"
+  label                  = var.environment
+  type                   = "kv"
+  value                  = random_string.ack_jwt_realm.result
+  depends_on = [
+    azurerm_role_assignment.appconf_dataowner
+  ]
+}
+
+resource "azurerm_app_configuration_key" "ack_jwt_secret" {
+  configuration_store_id = azurerm_app_configuration.app_conf.id
+  key                    = "JWT_SECRET"
+  label                  = var.environment
+  type                   = "vault"
+  vault_key_reference    = data.azurerm_key_vault_secret.jwt_secret.versionless_id
+  depends_on = [
+    azurerm_role_assignment.appconf_dataowner
+  ]
+}
