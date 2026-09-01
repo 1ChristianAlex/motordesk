c229ba2ceae47901a20307bb919f87770d62e881
Christian Alexsander Santos <47725581+1ChristianAlex@users.noreply.github.com>
2026-08-31 23:42:02 -0300

    fix(terraform): read secrets from existing key vault

--- infra/terraform/azure/keyvault-data.tf
+++ infra/terraform/azure/keyvault-data.tf
@@ -1,49 +1,49 @@
 data "azurerm_key_vault_secret" "database_user" {
   name         = "databaseUserv3"
-  key_vault_id = azurerm_key_vault.kvault_app.id
+  key_vault_id = data.azurerm_key_vault.kvault_app.id
 }
 
 data "azurerm_key_vault_secret" "database_password" {
   name         = "databasePasswordv3"
-  key_vault_id = azurerm_key_vault.kvault_app.id
+  key_vault_id = data.azurerm_key_vault.kvault_app.id
 }
 
 data "azurerm_key_vault_secret" "mongo_url" {
   name         = "mongoUrlv3"
-  key_vault_id = azurerm_key_vault.kvault_app.id
+  key_vault_id = data.azurerm_key_vault.kvault_app.id
 }
 
 data "azurerm_key_vault_secret" "mongo_user" {
   name         = "mongoUserv3"
-  key_vault_id = azurerm_key_vault.kvault_app.id
+  key_vault_id = data.azurerm_key_vault.kvault_app.id
 }
 
 data "azurerm_key_vault_secret" "mongo_password" {
   name         = "mongoPasswordv3"
-  key_vault_id = azurerm_key_vault.kvault_app.id
+  key_vault_id = data.azurerm_key_vault.kvault_app.id
 }
 
 data "azurerm_key_vault_secret" "mongo_database" {
   name         = "mongoDatabasev3"
-  key_vault_id = azurerm_key_vault.kvault_app.id
+  key_vault_id = data.azurerm_key_vault.kvault_app.id
 }
 
 data "azurerm_key_vault_secret" "redis_password" {
   name         = "redisPasswordv3"
-  key_vault_id = azurerm_key_vault.kvault_app.id
+  key_vault_id = data.azurerm_key_vault.kvault_app.id
 }
 
 data "azurerm_key_vault_secret" "azure_communication_access_key" {
   name         = "azureCommunicationAccessKeyv3"
-  key_vault_id = azurerm_key_vault.kvault_app.id
+  key_vault_id = data.azurerm_key_vault.kvault_app.id
 }
 
 data "azurerm_key_vault_secret" "azure_communication_endpoint" {
   name         = "azureCommunicationEndpointv3"
-  key_vault_id = azurerm_key_vault.kvault_app.id
+  key_vault_id = data.azurerm_key_vault.kvault_app.id
 }
 
 data "azurerm_key_vault_secret" "jwt_secret" {
   name         = "jwtSecretv3"
-  key_vault_id = azurerm_key_vault.kvault_app.id
+  key_vault_id = data.azurerm_key_vault.kvault_app.id
 }
